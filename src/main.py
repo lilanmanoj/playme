4b0bf73ebc528e66da185c3591031431322cd0a8
Lilan Manoj <lilanmanoj@adventus.io>
2022-04-09 18:03:33 +0530

    XML based playlists processing

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import argparse as ArgParse
 import source as Source
 import target as Target
 
+# Read arguments
 parser = ArgParse.ArgumentParser(
     prog='main',
     description='Read media from playlist and copy to destination')
@@ -9,8 +10,10 @@ parser.add_argument('source')
 parser.add_argument('destination')
 args = parser.parse_args()
 
+# Initiate playlist
 plst = Source.Playlist(args.source)
-plstMimeType = plst.identify()
+plstMimeType = plst.process()
 
+# Initiate target destination
 dest = Target.Destination(args.destination)
 dest.write()
--- src/source.py
+++ src/source.py
@@ -1,15 +1,25 @@
 import mimetypes as MimeTypes
-
+import xml.etree.ElementTree as ET
 class Playlist:
     file = ""
     mimeType = ""
 
     def __init__(self, file) -> None:
         self.file = file
+    
+    def process(self):
+        self.mimeType = self.get_mime()
+        
+        if self.mimeType == "application/xml":
+            tree = ET.parse(self.file)
+            root = tree.getroot()
 
-    def identify(self):
-        self.mimeType = MimeTypes.guess_type(self.file)
-        return self.mimeType
+            for playlist in root.findall('playlist'):
+                playlistTitle = playlist.find('title') or playlist.get('name')
+                print(playlistTitle)
+
+    def get_mime(self):
+        return MimeTypes.guess_type(self.file)
 
     def read(self):
         print(self.file)
